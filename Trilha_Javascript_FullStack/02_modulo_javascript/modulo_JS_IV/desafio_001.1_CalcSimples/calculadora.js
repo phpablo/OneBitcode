7d16a9d6bc421e27bb2a2449ec53e68030cae76e
phpablo <pablohenrique120@gmail.com>
2024-04-29 10:27:35 -0300

    :pencil: doc: Fixed structure of project
    - Organized files to stay equal the Course

---
+++ Trilha_Javascript_FullStack/02_modulo_javascript/modulo_JS_IV/desafio_001.1_CalcSimples/calculadora.js
@@ -0,0 +1,4 @@
+let num1 = parseFloat(prompt('Digite um número :'))
+let num2 = parseFloat(prompt('Digite outro número: '))
+
+alert(`A soma entre ${num1} e ${num2} é ${num1 + num2 } \n A subtração entre ${num1} e ${num2} é ${num1 - num2 } \n A multiplicação entre ${num1} e ${num2} é ${num1 * num2 } \n A divisão entre ${num1} e ${num2} é ${num1 / num2}`);
